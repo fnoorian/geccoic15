f94ee40a0bc428d9d5d510c8a11d62932575ef1a
Farzad Noorian <farzad.noorian@gmail.com>
2016-05-12 09:30:31 +1000

    Add missing train_all.R
    Thanks to nicornk for finding this

---
+++ train_all.R
@@ -0,0 +1,15 @@
+print("Number 1:")
+source("training/train1.R")
+
+print("Number 2")
+source("training/train2.R")
+
+print("Number 3")
+source("training/train3.R")
+
+print("Number 4")
+source("training/train4.R")
+
+print("Number 5")
+source("training/train5.R")
+
